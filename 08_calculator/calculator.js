0a92d27a3653432c61f8eb356939101f7d56d6f9
Sufyan Pawaskar <sufyanpawaskar@hotmail.com>
2024-04-12 12:34:44 +0530

    Completed the exercise calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(int1,int2) {
+  return int1+int2;
 };
 
-const subtract = function() {
-	
+const subtract = function(int1,int2) {
+	return int1-int2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,currItem)=>{
+    return total+currItem;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let xx = arr.reduce((total, currItem)=>{
+  return total*currItem;
+});
+  return xx
 };
 
-const power = function() {
-	
+const power = function(int1,int2) {
+	return int1**int2
 };
 
-const factorial = function() {
-	
+const factorial = function(int1) {
+  if (int1 == 1 || int1 == 0){
+    return 1;
+  } else{
+    return (int1 * factorial(int1-1))
+  }
 };
 
 // Do not edit below this line
