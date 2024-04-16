8abb526f8c65165eba9fa09cd587ad59f09b1010
Sufyan Pawaskar <sufyanpawaskar@hotmail.com>
2024-04-16 19:21:08 +0530

    completed the fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num1) {
+    let num = parseInt(num1)
+    let fib0 = 0;
+    let fib1 = 1;
+    if (num == 0){
+        return fib0;
+    } else if (num == 1){
+        return fib1;
+    } else if (num > 1){
+        for( let j = 2; j<num+1;j++){
+            let fib = fib0+fib1;
+            fib0 = fib1;
+            fib1 = fib;
+        }
+        return fib1;
+    }else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
