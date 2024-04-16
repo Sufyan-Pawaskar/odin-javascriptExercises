9e7473c1c4b84ac766db876922de7b73926cf045
Sufyan Pawaskar <sufyanpawaskar@hotmail.com>
2024-04-16 20:42:47 +0530

    Completed the Find The Oldest using reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(passedArr) {
+    let arr = passedArr.reduce((final, currItem)=>{
+        let obj = currItem;
+        let currAgeDiff = 0
+        let prevDiff = 0;
+        if("yearOfDeath" in obj){
+            currAgeDiff = obj.yearOfDeath - obj.yearOfBirth;
+        } else{
+            const today = new Date();
+            const year = today.getUTCFullYear();
+            currAgeDiff = year - obj.yearOfBirth;
+        }
+        if(final){
+            if("yearOfDeath" in final){
+                prevDiff = final.yearOfDeath - final.yearOfBirth;
+            } else{
+                const today1 = new Date();
+                const year1 = today1.getUTCFullYear();
+                prevDiff = year1 - final.yearOfBirth;
+            }
+            if(currAgeDiff > prevDiff){
+                console.log()
+                return obj;
+            } else{
+                return final;
+            }
+        } else{
+            return obj
+        }
 
+    })
+    return arr;
 };
 
 // Do not edit below this line
