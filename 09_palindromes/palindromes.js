286ccc38a0596a601935d9f656bad189c61d2f16
Sufyan Pawaskar <sufyanpawaskar@hotmail.com>
2024-04-16 18:51:53 +0530

    Completed the Palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const punct = [",",'""',";",":","."," ","!"]
+    let input = str.split('')
+    let inp1 = []
+    let inp2 = []
+    for (let i = 0; i<input.length;i++){
+        if (!punct.includes(input[i])){
+            inp1.push(input[i].toLowerCase());
+            inp2.push(input[i].toLowerCase());
+        }
+    }
+    let rev = inp2.reverse()
+    let ret = inp1.join() === rev.join()
+    return ret;
 };
 
 // Do not edit below this line
